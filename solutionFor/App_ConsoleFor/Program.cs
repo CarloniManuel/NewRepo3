b3b8119054569c72303fed5a64b16a05c5d3c9d0
studenti <studenti@LAB2-PC-17>
2018-01-22 09:21:32 +0100

    ho f

---
+++ solutionFor/App_ConsoleFor/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_ConsoleFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+            string risposta;
+            Console.WriteLine("inserisca quante volte vuole far ripetere la risposta");
+            numero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("inserisca la risposta");           
+            risposta = Console.ReadLine();
+            for (int i = 0; i < numero; i++)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("{0}",risposta);
+            }
+            Console.ReadLine();
+        }
+    }
+}
